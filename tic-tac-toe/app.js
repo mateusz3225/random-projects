f976ac28c47f3b1b9ce8a744598ee825c62d99f0
mateusz3225 <mateusz3225@email.com>
2025-09-05 16:12:05 +0200

    changed to 'class methods'

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -1,36 +1,33 @@
-//gameboard as an array inside of the Gameboard oject
-//another object to keep track of the players
 const textareaofaplayername1 = document.getElementById("insert-playername1");
 const textareaofaplayername2 = document.getElementById("insert-playername2");
 const currentplayertext = document.getElementById("current-player");
 const gridCells = document.getElementsByClassName("cell");
 const resetButton = document.querySelector(".reset-button");
 const displayDiv = document.querySelector(".result-display");
-const Gameboard = ( function () {
-const fillGameboard = () => {
+let gameboard = [];
+class Gameboard {
+static fillGameboard() {
     gameboard = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
 }
-const ShowDisplay = () => {
+static ShowDisplay() {
     displayDiv.innerHTML = `The winner is ${player1.playerName} [X]`;
 }
-const Player = (name, marker) => {
-    const playerName=name;
-    const playerMarker=marker;
-    return {playerName, playerMarker};
+constructor(name, marker) {
+    this.playerName = name;
+    this.playerMarker = marker;
 }
-const insert = (position,player) => {
-    
+static insert(position,player) {
     if (gameboard[position]==="-") {
         gameboard[position] = player.playerMarker;
     } Gameboard.Consoledisplaygameboard();
 }
-const show = () => {console.log(gameboard)}
-const Consoledisplaygameboard = () => {
+show() {console.log(gameboard)}
+static Consoledisplaygameboard() {
     console.log (` ${gameboard[0]} | ${gameboard[1]} | ${gameboard[2]} `);
     console.log (` ${gameboard[3]} | ${gameboard[4]} | ${gameboard[5]} `);
     console.log (` ${gameboard[6]} | ${gameboard[7]} | ${gameboard[8]} `);
 }
-const CheckforWinner = () => {
+static CheckforWinner() {
 const Tableofwinning = [
     [0, 1, 2],
     [3, 4, 5],
@@ -41,7 +38,7 @@ const Tableofwinning = [
     [0, 4, 8],
     [2, 4, 6]
 ];
-for (eachTable of Tableofwinning) {
+for (const eachTable of Tableofwinning) {
     if (gameboard[eachTable[0]] === gameboard[eachTable[1]] && gameboard[eachTable[1]] === gameboard[eachTable[2]]) {
         if(gameboard[eachTable[0]] != "-") {
             if (anumber===0)
@@ -50,28 +47,27 @@ for (eachTable of Tableofwinning) {
             displayDiv.innerHTML += `<p style="padding: 0px;">The winner is ${player1.playerName} [X]</p>`;
             gameboard = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
             anumber = 0;
-            for (cells of gridCells) {
+            for (const cells of gridCells) {
                 cells.innerText = "";
             }
         }
     }
 }};
-const fullboard = () => {
-    if (gameboard.every(cell => cell !== "-")) {
+static fullboard(){
+    if (gameboard.every((cell) => cell !== "-")) {
         displayDiv.innerHTML += `<p style="padding: 0px;">draw!</p>`;
         gameboard = ["-", "-", "-", "-", "-", "-", "-", "-", "-"];
             anumber = 0;
-            for (cells of gridCells) {
+            for (const cells of gridCells) {
                 cells.innerText = "";
             }
     }
 };
 
-return {fillGameboard, Player, insert, show, Consoledisplaygameboard, CheckforWinner, fullboard};
-})();
+};
 Gameboard.fillGameboard();
-const player1 = Gameboard.Player("player1", "X");
-const player2 = Gameboard.Player("player2", "O");
+const player1 = new Gameboard("player1", "X");
+const player2 = new Gameboard("player2", "O");
 let anumber=0;
 textareaofaplayername1.addEventListener("input", (e) => {
     player1.playerName = e.target.value;
@@ -108,10 +104,13 @@ textareaofaplayername2.addEventListener("input", (e) => {
 
 resetButton.addEventListener("click", () => {
     Gameboard.fillGameboard();
-    for (cells of gridCells) {
+    for (const cells of gridCells) {
         cells.innerText = "";
     }
     anumber = 0;
     currentplayertext.innerText = `${player1.playerName} [${player1.playerMarker}]`;
 });
 
+console.log(player1 
++ player2
+);
